a11fe98c7213cf21be656a85bac7dd7023922587
Alexandr <alexzaykov@gmail.com>
2018-05-27 22:43:56 +0500

    Фикс проекта

--- Project1.cpp
+++ Project1.cpp
@@ -4,14 +4,11 @@
 #pragma hdrstop
 #include <tchar.h>
 // ---------------------------------------------------------------------------
-USEFORM(
-	"Unit1.cpp",
-	mainForm );
-USEFORM(
-	"SaveKeyWindow.cpp",
-	Form2 );
-
-// ---------------------------------------------------------------------------
+USEFORM("Unit1.cpp", mainForm);
+USEFORM("SaveKeyWindow.cpp", Form2);
+USEFORM("SettingsWindow.cpp", Form3);
+USEFORM("GenerateKeys.cpp", Form4);
+//---------------------------------------------------------------------------
 int WINAPI _tWinMain(
 	HINSTANCE,
 	HINSTANCE,
@@ -22,9 +19,10 @@ int WINAPI _tWinMain(
 	{
 		Application->Initialize( );
 		Application->MainFormOnTaskBar = true;
-		Application->CreateForm( __classid( TmainForm ), & mainForm );
-		Application->CreateForm( __classid( TForm2 ), & Form2 );
-		// Application->CreateForm(__classid(TmainForm), &mainForm);
+		Application->CreateForm(__classid(TmainForm), &mainForm);
+		Application->CreateForm(__classid(TForm2), &Form2);
+		Application->CreateForm(__classid(TForm3), &Form3);
+		Application->CreateForm(__classid(TForm4), &Form4);
 		Application->Run( );
 	}
 	catch ( Exception & exception )
